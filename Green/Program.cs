17200bc16c3288d2b596bc8e151a92986296f95c
JanWu <wagrowskij@gmail.com>
2020-08-31 16:56:31 +0200

    Initial Commit

---
+++ Green/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Green
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Przywitanie info ze pobieramy imie
+            Console.WriteLine("What is your name, Cupcake?");
+            // Pobranie informacji o graczu
+            string name = Console.ReadLine();
+
+            //Sprawdz czy podal imie
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("I will still call you Cupcake, then.");
+                name = "Cupcake";
+            }
+            else if (name.ToLower() == "cupcake")
+            {
+                Console.WriteLine("Ha! I knew it.");
+                name = "Cupcake";
+            }
+
+            Console.WriteLine("Where are you from, " + name + "?");
+            string place = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(place))
+            {
+                Console.WriteLine("Fine, dont tell me.");
+                place = ("Nowhere");
+            }
+
+            // Wyswietl wiadomosc z imienienm
+            Console.WriteLine($"Welcome to Green, {name} from {place}!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey(true);
+
+            string[] level = {
+              "##########",
+              "#     #  #",
+              "#    ##  #",
+              "#     #  #",
+              "#        #",
+              "#     #  #",
+              "##########"
+
+            };
+                    string[] scroll = {
+            "    ____________________",
+            "  =(__   ___     __    _)=",
+            "    |                  |",
+            "    |__   ___  __   ___|   ",
+            "   =(____________________)="
+            };
+            Console.Clear();
+            Console.WriteLine($"Wanna see the map {name}? Press any key untill it is revealed...");
+
+            //int lineIndex = 0;
+            //while (lineIndex < scroll.Length / 2+1){
+            //  Console.WriteLine(scroll[lineIndex++]);
+            //}
+            for (int index = 0; index < scroll.Length / 2 + 1; index++)
+            {
+                Console.WriteLine(scroll[index]);
+            }
+            /*
+            if (lineIndex < scroll.Length) {
+              Console.WriteLine(scroll[lineIndex++]);
+            }
+            if (lineIndex < scroll.Length){
+              Console.WriteLine(scroll[lineIndex++]);
+            }
+            if (lineIndex < scroll.Length) {
+              Console.WriteLine(scroll[lineIndex++]);
+            }
+            */
+            /*  Console.WriteLine(scroll[lineIndex]);
+              lineIndex = lineIndex + 1;
+              Console.WriteLine(scroll[lineIndex]);
+              lineIndex++;
+              Console.WriteLine(scroll[lineIndex]);
+              Console.WriteLine(scroll[lineIndex++]);
+              */
+
+
+            int nextMapRowPosition = Console.CursorTop;
+            foreach (string row in level)
+            {
+                Console.SetCursorPosition(0, nextMapRowPosition);
+                Console.WriteLine($"    |    {row}    |");
+                for (int index = scroll.Length / 2 + 1; index < scroll.Length; index++)
+                {
+                    Console.WriteLine(scroll[index]);
+                }
+                nextMapRowPosition++;
+                Console.ReadKey(true);
+            }
+            //for (int index = scroll.Length /2+1; index < scroll.Length; index++){
+            //  Console.WriteLine(scroll[index]);
+            // Console.ReadKey(true); 
+            //}
+
+            //lineIndex = scroll.Length /2+1;
+            // while (lineIndex < scroll.Length) {
+            //   Console.WriteLine(scroll[lineIndex++]);
+            // }
+        }
+    }
+}
