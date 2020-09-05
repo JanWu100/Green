9e8bd2db8d1539cad71abc5a4fee15d726575d29
JanWu <wagrowskij@gmail.com>
2020-09-05 12:23:27 +0200

    Add level boundries and position resetting, figuring out collision system

--- Green/Program.cs
+++ Green/Program.cs
@@ -6,12 +6,9 @@ namespace Green
     {
         static void Main(string[] args)
         {
-            // Przywitanie info ze pobieramy imie
+            // Player creation
             Console.WriteLine("What is your name, Cupcake?");
-            // Pobranie informacji o graczu
             string name = Console.ReadLine();
-
-            //Sprawdz czy podal imie
             if (string.IsNullOrWhiteSpace(name))
             {
                 Console.WriteLine("I will still call you Cupcake, then.");
@@ -22,7 +19,6 @@ namespace Green
                 Console.WriteLine("Ha! I knew it.");
                 name = "Cupcake";
             }
-
             Console.WriteLine("Where are you from, " + name + "?");
             string place = Console.ReadLine();
             if (string.IsNullOrWhiteSpace(place))
@@ -30,12 +26,9 @@ namespace Green
                 Console.WriteLine("Fine, dont tell me.");
                 place = ("Nowhere");
             }
-
-            // Wyswietl wiadomosc z imienienm
             Console.WriteLine($"Welcome to Green, {name} from {place}!");
             Console.WriteLine("Press any key to continue");
             Console.ReadKey(true);
-
             string[] level = {
               "##########",
               "#     #  #",
@@ -56,34 +49,11 @@ namespace Green
             Console.Clear();
             Console.WriteLine($"Wanna see the map {name}? Press any key untill it is revealed...");
 
-            //int lineIndex = 0;
-            //while (lineIndex < scroll.Length / 2+1){
-            //  Console.WriteLine(scroll[lineIndex++]);
-            //}
             for (int index = 0; index < scroll.Length / 2 + 1; index++)
             {
                 Console.WriteLine(scroll[index]);
             }
-            /*
-            if (lineIndex < scroll.Length) {
-              Console.WriteLine(scroll[lineIndex++]);
-            }
-            if (lineIndex < scroll.Length){
-              Console.WriteLine(scroll[lineIndex++]);
-            }
-            if (lineIndex < scroll.Length) {
-              Console.WriteLine(scroll[lineIndex++]);
-            }
-            */
-            /*  Console.WriteLine(scroll[lineIndex]);
-              lineIndex = lineIndex + 1;
-              Console.WriteLine(scroll[lineIndex]);
-              lineIndex++;
-              Console.WriteLine(scroll[lineIndex]);
-              Console.WriteLine(scroll[lineIndex++]);
-              */
-
-
+          
             int nextMapRowPosition = Console.CursorTop;
             foreach (string row in level)
             {
@@ -96,21 +66,14 @@ namespace Green
                 nextMapRowPosition++;
                 Console.ReadKey(true);
             }
-            //for (int index = scroll.Length /2+1; index < scroll.Length; index++){
-            //  Console.WriteLine(scroll[index]);
-            // Console.ReadKey(true); 
-            //}
 
-            //lineIndex = scroll.Length /2+1;
-            // while (lineIndex < scroll.Length) {
-            //   Console.WriteLine(scroll[lineIndex++]);
-            // }
             Console.Clear();
+
+
             foreach (string row in level)
             {
                 Console.WriteLine(row);
             }
-
 
             int playerColumn = 2;
             int playerRow = 3;
@@ -119,23 +82,47 @@ namespace Green
             {
                 Console.SetCursorPosition(playerColumn, playerRow);
                 Console.Write("@");
-
+              
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                Console.SetCursorPosition(playerColumn, playerRow);
+                string currentRow = level[playerRow];
+                char currentCell = currentRow[playerColumn];
+                
+
+                Console.Write(currentCell);
+                int targetColumn = playerColumn;
+                int targetRow = playerRow;
+
+                if (keyInfo.Key == ConsoleKey.LeftArrow && currentRow[playerColumn-1] != '#')
                 {
-                    playerColumn--;
+                    targetColumn = playerColumn-1;
                 }
-                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                else if (keyInfo.Key == ConsoleKey.RightArrow && currentRow[playerColumn+1] != '#')
                 {
-                    playerColumn++;
+                    targetColumn = playerColumn+1;
                 }
-                else if (keyInfo.Key == ConsoleKey.UpArrow)
+                else if (keyInfo.Key == ConsoleKey.UpArrow && currentRow[playerColumn] != '#')
                 {
-                    playerRow--;
+                    targetRow = playerRow-1;
                 }
-                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                else if (keyInfo.Key == ConsoleKey.DownArrow && currentRow[playerColumn] != '#')
                 {
-                    playerRow++;
+                    targetRow = playerRow+1;
+                }
+                // Resetting position
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    targetColumn = 2;
+                    targetRow = 3;
+                }
+
+                if (targetColumn >= 0 && targetColumn < level[playerRow].Length)
+                {
+                    playerColumn = targetColumn;
+                }
+                if (targetRow >= 0 && targetRow < level.Length)
+                {
+                    playerRow = targetRow;
                 }
             }
             Console.SetCursorPosition(0, level.Length);
